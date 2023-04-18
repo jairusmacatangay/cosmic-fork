437ec521d1b5f33c46d8fc85f7c0f08cdafa292e
jairusmacatangay <54699680+jairusmacatangay@users.noreply.github.com>
2023-04-18 17:49:15 +0800

    update_user_migration and seed one user to users table (#4)
    * Added update_users_table migration
    
    * added seeding of one user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'johndoe@gmail.com',
+            'mobile_number' => '+639178172281',
+            'password' => Hash::make('Test1234!'),
+            'birthday' => '2000-01-01'
+        ]);
     }
 }
